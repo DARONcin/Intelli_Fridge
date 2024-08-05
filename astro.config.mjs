4a81fad155cae0ac6fdce9a74dfd8c24f9a2975c
Daron <141441005+DARONcin@users.noreply.github.com>
2024-08-04 19:33:55 -0600

    ReactAdd

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
-
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
+// No intentes usar AstroIntegration si no es compatible
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind(), react()]
+  // Otras configuraciones si es necesario
 });
---
+++ types/auth-astro.d.ts
@@ -0,0 +1,5 @@
+declare module "auth-astro/components" {
+    export const SignIn: any; 
+    export const SignOut: any; 
+  }
+  
