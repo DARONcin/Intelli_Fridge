7844000d6a383fafeb4204d35f4b7ae83b7cc46e
Daron <141441005+DARONcin@users.noreply.github.com>
2024-08-06 23:31:16 -0600

    vercel implementado?

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,14 @@ import tailwind from '@astrojs/tailwind';
 // No intentes usar AstroIntegration si no es compatible
 import react from "@astrojs/react";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()]
   // Otras configuraciones si es necesario
+  ,
+  output: "server",
+  adapter: vercel()
 });
+
